05e1a97919a0c2878351a84cd434e750ba317a0e
Matheus Alexandre <matheuscurses@gmail.com>
2022-07-13 22:54:01 -0300

    18. Input

---
+++ aula1.py
@@ -0,0 +1,10 @@
+nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+ano_nascimento = 2022 - int (idade)  # casting
+
+print()
+print(f'O seu nome é {nome} e vc tem {idade} anos.'
+      f' {nome} nasceu em {ano_nascimento}.')
+
+
+
---
+++ calculadora.py
@@ -0,0 +1,12 @@
+numero_1 = input("Digite um número: ")
+numero_2 = input("Digite outro número: ")
+resultado = float(numero_1) + float(numero_2)
+resultado2 = float(numero_1) - float(numero_2)
+resultado3 = float(numero_1) * float(numero_2)
+resultado4 = float(numero_1) / float(numero_2)
+
+print()
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
